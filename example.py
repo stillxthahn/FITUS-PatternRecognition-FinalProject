e15b678acbbb1b24b6794608de7ea587a2cdfcb4
stillxthahn <lxthanh235@gmail.com>
2025-04-06 19:03:03 +0700

    update: requirements

--- example.py
+++ example.py
@@ -86,13 +86,13 @@ with open(FACENET_FAIL_FILE, "w") as facenet_fail, open(DLIB_FAIL_FILE, "w") as 
                 dlib_param_lst, dlib_roi_box_lst = tddfa(img2, dlib_boxes)
 
                 # Landmark visualization
-                ver_landmark = tddfa.recon_vers(fn_param_lst, fn_roi_box_lst, dense_flag=False)
-                path_imglandmark = os.path.join(PATH_SAVE, f"{filename}_facenet_landmarks.jpg")
-                draw_landmarks(img2, ver_landmark, show_flag=False, dense_flag=False, wfp=path_imglandmark)
+                # ver_landmark = tddfa.recon_vers(fn_param_lst, fn_roi_box_lst, dense_flag=False)
+                # path_imglandmark = os.path.join(PATH_SAVE, f"{filename}_facenet_landmarks.jpg")
+                # draw_landmarks(img2, ver_landmark, show_flag=False, dense_flag=False, wfp=path_imglandmark)
 
-                ver_landmark = tddfa.recon_vers(dlib_param_lst, dlib_roi_box_lst, dense_flag=False)
-                path_imglandmark = os.path.join(PATH_SAVE, f"{filename}_dlib_landmarks.jpg")
-                draw_landmarks(img2, ver_landmark, show_flag=False, dense_flag=False, wfp=path_imglandmark)
+                # ver_landmark = tddfa.recon_vers(dlib_param_lst, dlib_roi_box_lst, dense_flag=False)
+                # path_imglandmark = os.path.join(PATH_SAVE, f"{filename}_dlib_landmarks.jpg")
+                # draw_landmarks(img2, ver_landmark, show_flag=False, dense_flag=False, wfp=path_imglandmark)
 
                 # Dense face reconstruction
                 fn_ver_3d = tddfa.recon_vers(fn_param_lst, fn_roi_box_lst, dense_flag=True)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+                # ver_landmark = tddfa.recon_vers(fn_param_lst, fn_roi_box_lst, dense_flag=False)
+                # path_imglandmark = os.path.join(PATH_SAVE, f"{filename}_facenet_landmarks.jpg")
+                # draw_landmarks(img2, ver_landmark, show_flag=False, dense_flag=False, wfp=path_imglandmark)
+
+                # ver_landmark = tddfa.recon_vers(dlib_param_lst, dlib_roi_box_lst, dense_flag=False)
+                # path_imglandmark = os.path.join(PATH_SAVE, f"{filename}_dlib_landmarks.jpg")
+                # draw_landmarks(img2, ver_landmark, show_flag=False, dense_flag=False, wfp=path_imglandmark)
